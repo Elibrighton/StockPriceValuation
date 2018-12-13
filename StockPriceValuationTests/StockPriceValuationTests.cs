824e9ac77d90b8afc4ab1c3296da2dccced98012
Eli Brighton <eli.brighton@gmail.com>
2018-12-13 12:14:24 +1000

    Remove Selenium dependancy

--- StockPriceValuationTests/StockPriceValuationTests.cs
+++ StockPriceValuationTests/StockPriceValuationTests.cs
@@ -7,25 +7,14 @@ namespace StockPriceValuationTests
     [TestClass]
     public class StockPriceValuationTests
     {
-        private Selenium _testSelenium;
-        private string _testUrl;
-
         [TestInitialize]
         public void Initialise()
         {
-            _testUrl = "https://quickfs.net/company/GOOG";
-            _testSelenium = new Selenium(_testUrl);
         }
 
         [TestMethod]
-        public void SeleniumGetElementTest()
+        public void TestMethod1()
         {
-            var testTagName = "div";
-            var testInnerText = "Overview";
-
-            var element = _testSelenium.GetElementByText(testTagName, testInnerText);
-
-            Assert.IsNotNull(element);
         }
     }
 }
